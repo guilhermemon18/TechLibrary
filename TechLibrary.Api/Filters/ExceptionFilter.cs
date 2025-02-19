55a38e3e4bfb36036af61613c9a80dedebcc39db
guilhermemon18 <guilhermemonsani7@gmail.com>
2025-02-19 15:53:23 -0300

    feat: Create ExceptionFilter

---
+++ TechLibrary.Api/Filters/ExceptionFilter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using TechLibrary.Communication.Responses;
+using TechLibrary.Exception;
+
+namespace TechLibrary.Api.Filters
+{
+    //Filtro de excessão que centraliza o tratamento de excessões da API e devolve as mensagens de erros adequadas ao usuário.
+    public class ExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            if(context.Exception is TechLibraryException techLibraryException)
+            {
+                context.HttpContext.Response.StatusCode = (int) techLibraryException.GetStatusCode();
+                context.Result = new ObjectResult( new ResponseErrorMessagesJson
+                {
+                    Errors = techLibraryException.GetErrorMessages()
+                });
+            }
+            else
+            {
+                context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                context.Result = new ObjectResult(new ResponseErrorMessagesJson
+                {
+                    Errors = ["Erro Desconhecido."]
+                });
+            }
+        }
+    }
+}
