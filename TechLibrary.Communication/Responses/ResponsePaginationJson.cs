13da13bffc3e193a02d7b9fc0caa6f8f5246b8f0
guilhermemon18 <guilhermemonsani7@gmail.com>
2025-02-19 20:33:51 -0300

    feat: create ResponsePaginationJson

---
+++ TechLibrary.Communication/Responses/ResponsePaginationJson.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechLibrary.Communication.Responses
+{
+    public class ResponsePaginationJson
+    {
+        public int PageNumber { get; set; }
+        public int TotalCount { get; set; }
+    }
+}
