08aebc71a2d7266de1c8e761157a2705ae8e4aa7
guilhermemon18 <guilhermemonsani7@gmail.com>
2025-02-19 15:00:15 -0300

    feat: create invalidLoginException

---
+++ TechLibrary.Exception/InvalidLoginException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechLibrary.Exception
+{
+    public class InvalidLoginException : TechLibraryException
+    {
+        public override List<string> GetErrorMessages() => ["Email e/ou senha inválidos."];
+
+
+        public override HttpStatusCode GetStatusCode() => HttpStatusCode.Unauthorized;
+    }
+}
