a39f76cc751c3acff17de72206074899e5f56d0e
guilhermemon18 <guilhermemonsani7@gmail.com>
2025-02-19 20:45:30 -0300

    feat: add Book entity to DbContext

--- TechLibrary.Api/Infraestructure/DataAccess/TechLibraryDbContext.cs
+++ TechLibrary.Api/Infraestructure/DataAccess/TechLibraryDbContext.cs
@@ -7,6 +7,7 @@ namespace TechLibrary.Api.Infraestructure.DataAccess
     {
 
         public DbSet<User> Users { get; set; }
+        public DbSet<Book> Books { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=../techlibrary.db");
