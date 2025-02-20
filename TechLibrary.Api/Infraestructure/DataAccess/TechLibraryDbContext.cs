61e3889c7cc06632a8a254fcc4a6486de5ddaac8
guilhermemon18 <guilhermemonsani7@gmail.com>
2025-02-20 01:20:51 -0300

    feat: add entity Checkout to DBContext

--- TechLibrary.Api/Infraestructure/DataAccess/TechLibraryDbContext.cs
+++ TechLibrary.Api/Infraestructure/DataAccess/TechLibraryDbContext.cs
@@ -8,6 +8,7 @@ namespace TechLibrary.Api.Infraestructure.DataAccess
 
         public DbSet<User> Users { get; set; }
         public DbSet<Book> Books { get; set; }
+        public DbSet<Checkout> Checkouts { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=../techlibrary.db");
