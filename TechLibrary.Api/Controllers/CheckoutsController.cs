a94b2c2833a3be29ca952604f23a6b22c24c81b2
guilhermemon18 <guilhermemonsani7@gmail.com>
2025-02-20 01:26:59 -0300

    feat: create CheckoutsController

---
+++ TechLibrary.Api/Controllers/CheckoutsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TechLibrary.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CheckoutsController : ControllerBase
+    {
+        [HttpPost]
+        //Define que o bookId virá pela rota, como boa prática IDS SEMPRE devem vir pela rota, não como query params.
+        [Route("{bookId}")]
+        public IActionResult BookCheckout(Guid bookId)
+        {
+
+
+            return NoContent();
+        }
+    }
+}
