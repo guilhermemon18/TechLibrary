ddbe312477aecbac62913a9669c6a4571093d48b
guilhermemon18 <guilhermemonsani7@gmail.com>
2025-02-20 03:10:08 -0300

    feat: create CheckoutsController

--- TechLibrary.Api/Controllers/CheckoutsController.cs
+++ TechLibrary.Api/Controllers/CheckoutsController.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using TechLibrary.Api.Services.LoggedUser;
+using TechLibrary.Api.UseCases.Checkouts;
 
 namespace TechLibrary.Api.Controllers
 {
     [Route("[controller]")]
     [ApiController]
+    [Authorize]
     public class CheckoutsController : ControllerBase
     {
         [HttpPost]
@@ -12,7 +15,11 @@ namespace TechLibrary.Api.Controllers
         [Route("{bookId}")]
         public IActionResult BookCheckout(Guid bookId)
         {
+            var loggedUser = new LoggedUserService(HttpContext);
 
+            var useCase = new RegisterBookCheckoutUseCase(loggedUser);
+
+            useCase.Execute(bookId);
 
             return NoContent();
         }
