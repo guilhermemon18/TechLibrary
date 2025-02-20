2981ca44e443d72fb00598c8de5f817f4fe12006
guilhermemon18 <guilhermemonsani7@gmail.com>
2025-02-20 02:18:54 -0300

    feat: config Authentication dependency

--- TechLibrary.Api/Program.cs
+++ TechLibrary.Api/Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 using TechLibrary.Api.Filters;
+
+
+const string AUTHENTICATION_TYPE = "Bearer";
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,10 +14,55 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//Configura o Swagger para realizar a autenticação com o TOKEN JWT:
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition(AUTHENTICATION_TYPE, new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme.
+                      Enter 'Bearer' [space] and then your token in the text input below;
+                      Example: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = AUTHENTICATION_TYPE
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = AUTHENTICATION_TYPE
+                },
+                Scheme = "oauth2",
+                Name = AUTHENTICATION_TYPE,
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
 
 //Adiciona o filtro que centraliza o tratamento de excessões da API.
 builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
+
+//Configura a autentição utilizando o token JWT:
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = SecurityKey()
+        };
+    });
 
 var app = builder.Build();
 
@@ -28,3 +80,12 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+SymmetricSecurityKey SecurityKey()
+{
+    var signingKey = "pxEU1gD8MnPFIM20CINYGvGyK6undjYZ";
+
+    var symmetricKey = Encoding.UTF8.GetBytes(signingKey);
+
+    return new SymmetricSecurityKey(symmetricKey);
+}
